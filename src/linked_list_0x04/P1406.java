e00256e11bb893ba1f78acc736ff386a42014f30
gol2580 <gol11971197@gmail.com>
2024-04-08 22:28:30 +0900

    2024-04-08 0x04 : P1406 - 성공

--- src/linked_list_0x04/P1406.java
+++ src/linked_list_0x04/P1406.java
@@ -4,45 +4,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class P1406 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         int n = Integer.parseInt(br.readLine());
+
         LinkedList<Character> list = new LinkedList();
-        for (int i = 0; i < str.length(); i++) {
-            list.add(str.charAt(i));
-        }
-        int cursor = list.size() - 1;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < str.length(); i++) {list.add(str.charAt(i));}
+
+        ListIterator<Character> iter = list.listIterator();
+        while(iter.hasNext()) {iter.next();}
+
+
+        for(int i=0;i<n;i++) {
             String[] command = br.readLine().split(" ");
             if (command[0].equals("P")) {
                 char inChar = command[1].charAt(0);
-                if (cursor == (list.size() - 1)) {
-                    list.addLast(inChar); cursor++;
-                } else if (cursor == -1) {
-                    list.addFirst(inChar); cursor++;
-                } else {
-                    list.add(++cursor,inChar);
-                }
+                iter.add(inChar);
             }
             if (command[0].equals("L")) {
-                if (cursor == -1) {continue;}
-                else { cursor--;}
+                if (iter.hasPrevious() == false) {continue;}
+                iter.previous();
             }
             if (command[0].equals("D")) {
-                if (cursor == (list.size() - 1)) {continue;}
-                else {cursor++;}
+                if (iter.hasNext()==false) {continue;}
+                iter.next();
             }
             if(command[0].equals("B")) {
-                if (cursor == (list.size() - 1)) {list.removeLast(); cursor--;}
-                else if (cursor == -1) {continue;}
-                else {list.remove(cursor);cursor--;}
+                if (iter.hasPrevious() == false) {continue;}
+                iter.previous(); iter.remove();
             }
         }
+        StringBuilder sb = new StringBuilder();
         for(char c : list) {
-            System.out.print(c);
+            sb.append(c);
         }
+        System.out.println(sb);
     }
 }
