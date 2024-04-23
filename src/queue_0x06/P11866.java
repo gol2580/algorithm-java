ea6421d35b9a1a8fedb9541d27dc0e13063856e7
gol2580 <gol11971197@gmail.com>
2024-04-23 23:05:45 +0900

    2024-04-23 0x06 : P11866

---
+++ src/queue_0x06/P11866.java
@@ -0,0 +1,28 @@
+package queue_0x06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class P11866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringBuilder sb = new StringBuilder().append("<");
+        Queue<Integer> q = new LinkedList<>();
+        int n = Integer.parseInt(st.nextToken()); int k = Integer.parseInt(st.nextToken());
+        for(int i=1;i<=n;i++) {q.offer(i);}
+        while(true) {
+            if(q.isEmpty()) {sb.append(">"); break;}
+            for(int i=1;;i++) {
+                if(i%3==0) { sb.append(q.poll()); break;}
+                else { q.offer(q.poll()); }
+            }
+            if(!q.isEmpty()) {sb.append(", ");}
+        }
+        System.out.println(sb);
+    }
+}
