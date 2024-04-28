514d709f9ddf23fb0ebc65929a67050a42259b58
gol2580 <gol11971197@gmail.com>
2024-04-29 01:03:22 +0900

    2024-04-29 0x06 : P1021 성공

--- src/deque_0x07/P1021.java
+++ src/deque_0x07/P1021.java
@@ -10,6 +10,7 @@ import java.util.StringTokenizer;
 public class P1021 {
     public static void main(String[] args) throws IOException {
         int cnt=0; int[] list = new int[50];
+        int tmpcnt=0; boolean success = false;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         int n = Integer.parseInt(st.nextToken()); int k = Integer.parseInt(st.nextToken());
@@ -20,17 +21,25 @@ public class P1021 {
         for(int i=1;i<=n;i++) {d.offerLast(i);}
 
         for(int i=0;i<k;i++) {
+            success=false; tmpcnt=0;
             int target = list[i];
             while(true) {
                 if(target==d.peekFirst()) { //1번연산
                     d.pollFirst();
                     break;
-                } else if() { //2번연산
-                    d.offerLast(d.pollFirst());
-                    cnt++;
-                } else if() { //3번연산
-                    d.offerFirst(d.pollLast());
-                    cnt++;
+                } else {
+                    for(int j=0;j<(d.size()/2+1);j++) { //덱 사이즈 반쪽만큼 왼쪽 츄라이츄라이
+                        if(target!=d.peekFirst()) {d.offerLast(d.pollFirst()); tmpcnt++;}
+                        else {success=true; cnt+=tmpcnt; break;}
+                    }
+                    if(!success) { //왼쪽 아니면
+                        for(int j=0;j<tmpcnt;j++) {d.offerFirst(d.pollLast());} //원상복구
+                        tmpcnt=0;
+                        for(int j=0;j<(d.size()/2+1);j++) { //오른쪽으로
+                            if(target!=d.peekFirst()) {d.offerFirst(d.pollLast()); tmpcnt++;}
+                            else {cnt+=tmpcnt; break;}
+                        }
+                    }
                 }
             }
         }
