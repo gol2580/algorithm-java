2f220fa3abaeba47c03ef6c9a4be57653f6f0259
gol2580 <gol11971197@gmail.com>
2024-05-06 19:50:40 +0900

    2024-05-06 0x09 : P2583 - 성공

--- src/BFS_0x09/P2583.java
+++ src/BFS_0x09/P2583.java
@@ -3,10 +3,7 @@ package BFS_0x09;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class P2583 {
     public static Queue<Pair> q = new LinkedList<>();
@@ -16,28 +13,32 @@ public class P2583 {
     public static int[] dy = {0,0,-1,1};
     public static Pair cur;
     public static int n; public static int m;
+    public static int cnt=0;
+    public static ArrayList<Integer> dists = new ArrayList();
 
     public static void BFS() {
+        int dist=0;
         while(!q.isEmpty()) {
-            cur = q.poll();
+            cur = q.poll(); dist++;
             for(int dir=0;dir<4;dir++) {
                 int nx = cur.x+dx[dir];
-                int ny = cur.y+dx[dir];
-                if(nx<0 || nx<=n || ny<0 || ny<=m) {continue;}
-                if(visited[nx][ny]!=-1 || board[nx][ny]==1) {continue;} //이미 방문햇거나 박스면
+                int ny = cur.y+dy[dir];
+                if(nx<0 || nx>=n || ny<0 || ny>=m) {continue;}
+                if(visited[nx][ny]==1 || board[nx][ny]==1) {continue;} //이미 방문햇거나 박스면
+                visited[nx][ny]= 1;
+                q.offer(new Pair(nx, ny));
             }
         }
-
-
+        dists.add(dist);
     }
 
     public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         n = Integer.parseInt(st.nextToken()); m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         board = new int[n][m]; visited = new int[n][m];
-        Arrays.fill(visited,-1);
 
         for(int i=0;i<k;i++) {
             int x1=0; int x2=0; int y1=0; int y2=0;
@@ -47,7 +48,6 @@ public class P2583 {
                 else if(j==1) {x1 = Integer.parseInt(st.nextToken());}
                 else if(j==2) {y2 = Integer.parseInt(st.nextToken())-1;}
                 else if(j==3) {x2 = Integer.parseInt(st.nextToken())-1;}
-
             }
             for(int j=x1;j<=x2;j++) {
                 for(int l=y1;l<=y2;l++) {
@@ -57,21 +57,19 @@ public class P2583 {
         }
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                if(board[i][j]==0  && visited[i][j]==-1) {
-                    q.offer(new Pair(i,j));
+                if(board[i][j]==0  && visited[i][j]==0) { //사각형이 아니고 && 방문하지 않은 곳
+                    q.offer(new Pair(i,j)); visited[i][j]=1;
                     BFS();
+                    cnt++;
                 }
             }
         }
-
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                System.out.print(board[i][j]);
-            }
-            System.out.println();
-        }
+        sb.append(cnt).append('\n');
+        Collections.sort(dists);
+        ListIterator<Integer> iter = dists.listIterator();
+        while(iter.hasNext()) {sb.append(iter.next()).append(' ');}
+        System.out.println(sb);
     }
-
     public static class Pair {
         int x; int y;
         Pair(int x, int y) {
