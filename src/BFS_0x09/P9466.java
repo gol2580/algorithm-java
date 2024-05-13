8952ba32e27b99e0717756d2bd6b7b17dbd4ec73
gol2580 <gol11971197@gmail.com>
2024-05-13 23:54:05 +0900

    2024-05-13 0x09 : P9466

--- src/BFS_0x09/P9466.java
+++ src/BFS_0x09/P9466.java
@@ -12,20 +12,29 @@ public class P9466 {
     public static int m;
     public static int result;
     public static int[] board;
-    public static int[] grouped;
+    public static int[] visited;
     public static Queue<Integer> q = new LinkedList<>();
-    public static int getGroup(int start) {
+    public static int getGroup() {
+        visited = new int[m];
         int members=0;
-        int cur=0; int next=0;
+        int cur=0; int next=0; int start =0;
         while(!q.isEmpty()) {
-            cur = q.poll(); members++;
+            cur = q.poll(); board[cur]=0;
             next = board[cur];
-            if(board[cur]==-1) {break;}
-            if(cur==next) {break;}
-            if(next==start) { result-=members; return -1; }
-            board[cur]=-1;
+            //이미 그룹이 지어진 사람이거나 or 방문했던 경우
+            if(visited[next]==1 || board[next]==-1 || board[next]==0) {break;}
+            //visited : 이번 싸이클에서의 방문여부 표시
+            if(visited[board[next]]==1) {
+                start = board[next]; // ->
+                cur = next;
+                q.offer(board[next]);
+                while(cur!=start) {
+                    cur = q.poll(); result--;
+                }
+            }
             q.offer(next);
         }
+
         return -1;
     }
 
@@ -37,13 +46,14 @@ public class P9466 {
             m = Integer.parseInt(br.readLine());
             result = m;
             st = new StringTokenizer(br.readLine()," ");
-            board = new int[m]; grouped = new int[m];
+            board = new int[m];
             for(int i=0;i<m;i++) {board[i]=Integer.parseInt(st.nextToken())-1;}
             for(int i=0;i<m;i++) {
-                if(board[i]==i) { result--; board[i]=-1; continue; }
-                if(board[i]==-1) { continue; }
+                if(board[i]==i) { result--; board[i]=-1; continue; } //board[i]==-1 : 이미 짝이 지어졌다
+                //이미 짝이 지어졌거나 방문했던 곳인 경우 (전 싸이클에서 검사했던 경우)
+                if(board[i]==-1 || board[i]==0) { continue; }
                 q.offer(i);
-                getGroup(i);
+                getGroup();
             }
             System.out.println(result);
         }
