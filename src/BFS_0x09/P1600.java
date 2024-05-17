8c9cc4c3476c34e7fd49f1858add47f8cc1ed881
gol2580 <gol11971197@gmail.com>
2024-05-18 01:50:02 +0900

    2024-05-18 0x09 : P1600 - 틀렸습니다

--- src/BFS_0x09/P1600.java
+++ src/BFS_0x09/P1600.java
@@ -8,30 +8,47 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class P1600 {
-    static int w, h, k;
-    static int[][] board, distance;
+    static int w, h, k, nx, ny;
+    static int[][] board; static int[][][] distance;
     static Pair cur;
+    static int result;
     static Queue<Pair> q = new LinkedList<>();
-    static int[] dx = {1,-1,0,0,2,1,-1,-2,2,1,-2,-1};
-    static int[] dy = {0,0,-1,1,1,2,2,1,-1,-2,-1,-2};
+    static int[] dx = {1,-1,0,0,2,1,-2,-1,2,1,-2,-1};
+    static int[] dy = {0,0,-1,1,1,2,1,2,-1,-2,-1,-2};
 
-    static void BFS() {
+    static int BFS() {
+        result = w*h;
+        if(w==1 && h==1) {
+            result=0;
+            return -1;
+        }
         while(!q.isEmpty()) {
             cur = q.poll();
             for(int dir=0;dir<12;dir++) {
-                int nx = cur.x+dx[dir];
-                int ny = cur.y+dy[dir];
+                nx = cur.x+dx[dir];
+                ny = cur.y+dy[dir];
 
                 if(nx<0 || nx>=h || ny<0 || ny>=w) {continue;}
-                if(dir>=4 && cur.cnt>k) {continue;}
+                if(dir>=4 && cur.cnt>=k) {continue;}
+                if(board[nx][ny]==1) {continue;}
+                //distance[x][y][0] : 거리 , distance[x][y][1] : 그 거리에 도달했을때 cnt
+                if(distance[cur.x][cur.y][1] >= distance[nx][ny][1] && distance[nx][ny][0]!=0) {continue;}
+                // 도착
+                if(nx==(h-1)&&ny==(w-1)) result = Math.min(result,distance[cur.x][cur.y][0]);
 
-                if(nx==(h-1)&&ny==(w-1)) { System.out.println(distance[cur.x][cur.y]+1); }
 
-                distance[nx][ny]=distance[cur.x][cur.y]+1;
-                if(dir>=4) q.offer(new Pair(nx,ny,cur.cnt+1));
-                else q.offer(new Pair(nx,ny, cur.cnt));
+                distance[nx][ny][0]=distance[cur.x][cur.y][0]+1;
+                if(dir>=4) {
+                    distance[nx][ny][1]=distance[cur.x][cur.y][1]+1;
+                    q.offer(new Pair(nx,ny,cur.cnt+1));
+                } else {
+                    distance[nx][ny][1]=distance[cur.x][cur.y][1];
+                    q.offer(new Pair(nx,ny, cur.cnt));
+                }
             }
         }
+        if(result==w*h) result=-1;
+        return -1;
     }
 
     public static void main(String[] args) throws IOException {
@@ -39,7 +56,7 @@ public class P1600 {
         k = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         w = Integer.parseInt(st.nextToken()); h = Integer.parseInt(st.nextToken());
-        board = new int[h][w]; distance = new int[h][w];
+        board = new int[h][w]; distance = new int[h][w][2];
         for (int i=0;i<h;i++) {
             st = new StringTokenizer(br.readLine()," ");
             for(int j=0;j<w;j++) {
@@ -47,7 +64,9 @@ public class P1600 {
             }
         }
         q.offer(new Pair(0,0,0));
+        distance[0][0][0] = 1; distance[0][0][1]=0;
         BFS();
+        System.out.println(result);
     }
 
     static class Pair {
