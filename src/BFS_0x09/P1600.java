b2be0c2c3e98463415e3aef6a7f8440eeef2ba96
gol2580 <gol11971197@gmail.com>
2024-05-19 23:57:32 +0900

    2024-05-19 0x09 : P1600

--- src/BFS_0x09/P1600.java
+++ src/BFS_0x09/P1600.java
@@ -32,19 +32,24 @@ public class P1600 {
                 if(dir>=4 && cur.cnt>=k) {continue;}
                 if(board[nx][ny]==1) {continue;}
                 //distance[x][y][0] : 거리 , distance[x][y][1] : 그 거리에 도달했을때 cnt
-                if(distance[cur.x][cur.y][1] >= distance[nx][ny][1] && distance[nx][ny][0]!=0) {continue;}
+                if(distance[nx][ny][cur.cnt]!=0) {continue;}
                 // 도착
-                if(nx==(h-1)&&ny==(w-1)) result = Math.min(result,distance[cur.x][cur.y][0]);
+                if(nx==(h-1)&&ny==(w-1)) result = distance[cur.x][cur.y][cur.cnt];
 
 
-                distance[nx][ny][0]=distance[cur.x][cur.y][0]+1;
                 if(dir>=4) {
-                    distance[nx][ny][1]=distance[cur.x][cur.y][1]+1;
                     q.offer(new Pair(nx,ny,cur.cnt+1));
+                    distance[nx][ny][cur.cnt+1]=distance[nx][ny][cur.cnt]+1;
                 } else {
-                    distance[nx][ny][1]=distance[cur.x][cur.y][1];
                     q.offer(new Pair(nx,ny, cur.cnt));
+                    distance[nx][ny][cur.cnt]+=1;
                 }
+                /*
+                System.out.println(nx+", "+ny+" : "+ distance[nx][ny][cur.cnt]);
+                System.out.println(nx+", "+ny+" : "+ distance[nx][ny][cur.cnt+1]);
+                System.out.println();
+
+                 */
             }
         }
         if(result==w*h) result=-1;
@@ -56,7 +61,7 @@ public class P1600 {
         k = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         w = Integer.parseInt(st.nextToken()); h = Integer.parseInt(st.nextToken());
-        board = new int[h][w]; distance = new int[h][w][2];
+        board = new int[h][w]; distance = new int[h][w][31];
         for (int i=0;i<h;i++) {
             st = new StringTokenizer(br.readLine()," ");
             for(int j=0;j<w;j++) {
@@ -64,7 +69,7 @@ public class P1600 {
             }
         }
         q.offer(new Pair(0,0,0));
-        distance[0][0][0] = 1; distance[0][0][1]=0;
+        distance[0][0][0] = 1;
         BFS();
         System.out.println(result);
     }
