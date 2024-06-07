d8ee8e98d88df545944177191b211970791106d0
gol2580 <gol11971197@gmail.com>
2024-06-07 22:51:37 +0900

    2024-06-07 0x10 : P2579 ?

--- src/DP_0x10/P2579.java
+++ src/DP_0x10/P2579.java
@@ -11,11 +11,22 @@ public class P2579 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] s = new int[n+5]; int[] d = new int[10005];
+        int[] s = new int[305]; int[][] d = new int[305][3];
         for (int i = 0; i < n; i++) s[i] = Integer.parseInt(br.readLine());
         // d[k][1] = Math.max(d[k-2][1], d[k-2][2]) + s[k]
         // d[k][2] = d[k-1][1] + s[k]
-
-
+        if(n==1) {
+            System.out.println(s[1]);
+            return;
+        }
+        d[1][1]= s[1];
+        d[1][2] = 0;
+        d[2][1] = s[2];
+        d[2][2] = s[1]+s[2];
+        for (int i = 3; i <= n ; i++) {
+            d[i][1] = Math.max(d[i-2][1], d[i-2][2]+s[i]);
+            d[i][2] = d[i-1][1]+s[i];
+        }
+        System.out.println(Math.max(d[n][1], d[n][2]));
     }
 }
