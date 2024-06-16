a4030f228d3d4721d81e7d220eaa8f46e254bbe9
gol2580 <gol11971197@gmail.com>
2024-06-16 14:02:18 +0900

    2024-06-16 0x10 : 업보청산

--- src/DP_0x10/업보청산.java
+++ src/DP_0x10/업보청산.java
@@ -5,9 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class 업보청산 {
-    /*
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        /*
         //1. 테이블정의
         //d[i] : d를 1로 만들기 위해 필요한 연산횟수
         int[] d = new int[1000005];
@@ -23,10 +24,28 @@ public class 업보청산 {
             if(i%2==0) d[i]=Math.min(d[i], d[i/2]+1);
         }
         System.out.println(d[n]);
-    }
-     */
+    */
+        int n = Integer.parseInt(br.readLine());
+        int[] s = new int[305];
+        for (int i = 1; i <= n; i++) s[i] = Integer.parseInt(br.readLine());
 
-    public static void main(String[] args) {
+        //1. 테이블정의
+        // d[i][3] : 계단을 밟았을 때 점수합의 최댓값, 2차원은 연속해서 n번 밟았을 때
+        int[][] d = new int[305][2];
 
+        //2. 초기값 설정
+        d[1][0] = s[1]; d[1][1] = s[1];
+        d[2][0] = s[2]; d[2][1] = s[1]+s[2];
+
+        //3. 점화식 찾기
+        //d[3][0] = Math.max(d[1][0], d[1][1])+s[3];
+        //d[3][1] = d[2][0]+s[3];
+        for (int i = 3; i <= n; i++) {
+            d[i][0] = Math.max(d[i-2][0], d[i-2][1])+s[i];
+            d[i][1] = d[i-1][0]+s[i];
+        }
+
+        System.out.println(Math.max(d[n][0], d[n][1]));
     }
+
 }
